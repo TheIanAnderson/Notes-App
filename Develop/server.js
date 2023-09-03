49b93bc6cbd521649a2b0d7dcf9568b4f13ba67b
TheIanAnderson <131201726+TheIanAnderson@users.noreply.github.com>
2023-09-03 12:23:25 -0500

    Added functioning server and router for index.html

---
+++ Develop/server.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const routes = require("./Controllers");
+// const sequelize = require("./config/connection");
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log("Now listening to localhost 3001");
+});
