d7b87ca1209ac39ea500dfa336a8bf60b109dd94
TheIanAnderson <131201726+TheIanAnderson@users.noreply.github.com>
2023-09-03 13:21:16 -0500

    Added get route for notes file and more server functionality

--- Develop/Controllers/index.js
+++ Develop/Controllers/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const homeRoutes = require("./homeRoutes.js");
+const notesRouter = require("./homeRoutes.js")
 
 router.use("/", homeRoutes);
+router.use("/api/notes", notesRouter)
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -8,6 +8,9 @@ const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/api/notes.html"));
+});
 
 app.use(routes);
 
